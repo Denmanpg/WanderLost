e34e36ba57badea6113443cfdc5e4e0f51f55e6c
Patricia Denman <denmanpg@gmail.com>
2016-10-31 11:21:18 -0400

    second commit

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
 
   devise_for :users
 
+  resources :blog_posts
+
   resources :users, only: [:show, :index]
 
   resources :friendships, only: [:create, :destroy, :accept] do
