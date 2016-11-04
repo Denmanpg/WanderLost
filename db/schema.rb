984ffeafeb0b7065d32937e0d01f05a84f0a3994
Jessica Swirat <soxfan1173@gmail.com>
2016-11-04 16:33:00 -0400

    2nd merge - JS

--- db/schema.rb
+++ db/schema.rb
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20161102171850) do
 
   create_table "blog_posts", force: :cascade do |t|
     t.string   "title"
+    t.string   "author"
     t.text     "blog_entry"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
