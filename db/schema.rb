ac3d6d87544f940f9b0a6ee1f8bc55afe176f045
Jessica Swirat <soxfan1173@gmail.com>
2016-11-08 18:40:05 -0500

    merge master file conflict

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161108211031) do
+ActiveRecord::Schema.define(version: 20161102171850) do
 
   create_table "activities", force: :cascade do |t|
     t.string   "trackable_type"
@@ -36,12 +36,6 @@ ActiveRecord::Schema.define(version: 20161108211031) do
     t.datetime "updated_at", null: false
     t.integer  "user_id"
     t.index ["user_id"], name: "index_blog_posts_on_user_id"
-  end
-
-  create_table "categories", force: :cascade do |t|
-    t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "comments", force: :cascade do |t|
@@ -131,21 +125,6 @@ ActiveRecord::Schema.define(version: 20161108211031) do
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "products", force: :cascade do |t|
-    t.string   "name"
-    t.text     "description"
-    t.integer  "category_id"
-    t.integer  "user_id"
-    t.datetime "created_at",         null: false
-    t.datetime "updated_at",         null: false
-    t.string   "image_file_name"
-    t.string   "image_content_type"
-    t.integer  "image_file_size"
-    t.datetime "image_updated_at"
-    t.index ["category_id"], name: "index_products_on_category_id"
-    t.index ["user_id"], name: "index_products_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
